b689d4c0372c5d7273843c80cb3474ca23f275ad
Steve Koch <stevekochscience@gmail.com>
2012-04-13 09:39:20 -0600

    cleaned up a tiny bit, added credits to help pages, and added some comments

--- spell.py
+++ spell.py
@@ -1,14 +1,16 @@
+# pygame help from http://code.activestate.com/recipes/521884-play-sound-files-with-pygame-in-a-cross-platform-m/ (for playing sounds)
+# shuffle help from http://stackoverflow.com/a/976921/868718
+
 import sys
-import pygame
-from random import shuffle
+import pygame               # This library required for playing audio files
+from random import shuffle  # These two functions for shuffling the word list
 from random import random
 
 FREQ = 44100    # same as audio CD
 BITSIZE = -16   # unsigned 16 bit
 CHANNELS = 2    # 1 == mono, 2 == stereo
 BUFFER = 1024   # audio buffer size in no. of samples
-FRAMERATE = 30
-HH = 3
+FRAMERATE = 30  # This has something to do with how often to check if sound is done?
 
 def playsound(soundfile):
     """Play sound through default mixer channel in blocking manner.
@@ -23,14 +25,16 @@ def playsound(soundfile):
         clock.tick(FRAMERATE)
 
 def main(args):
-
+    """For starters, I just hacked the main function from the above pygame sound tutorial.  I know this should be split into functions, but for now it works. I also don't understand the error checking and I don't think it works
+    """
+    
     try:
         pygame.mixer.init(FREQ, BITSIZE, CHANNELS, BUFFER)
     except pygame.error, exc:
         print >>sys.stderr, "Could not initialize sound system: %s" % exc
         return 1
     
-    wordfile = open("2012bee.txt")
+    wordfile = open("2012bee.txt") # for now this file is hard-coded.  Should be sent in as an argument
 
     wordfilelist = wordfile.readlines()
     shuffle(wordfilelist, random)
@@ -41,7 +45,7 @@ def main(args):
         i += 1
         print "\n\n\nword %d out of %d" % (i, len(wordfilelist))
         while True:
-            correct, this, that = sfiles.partition('.')
+            correct, this, that = sfiles.partition('.') # I am using this and that to store the parts of strings i don't want
             soundfile, this, that = sfiles.partition('\n')
             # print "correct is %s" % correct
             # print soundfile
@@ -74,8 +78,8 @@ def main(args):
 
         
     return 0
-    
-if __name__ == '__main__':
+# this is from the pygame sound tutorial. I don't really know what it does.    
+if __name__ == '__main__':         
     sys.exit(main(sys.argv[1:]))
     
        
